9ad1a8350818c61afa9dc0e160e1f3b2a2a522a8
anatoly <anspesiv@yahoo.com>
2020-09-27 20:21:09 +0300

    stateful widget created

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,21 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
   var questionIndex = 0;
 
   void answerQuestion() {
-    questionIndex++;
+    setState(() {
+      questionIndex++;
+    });
+
     print("Q index is " + questionIndex.toString());
   }
 
